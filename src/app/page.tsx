c849d31c71d7946f386b4bd5421113289cdd3a66
Kobby00 <kwabenaoware243@gmail.com>
2024-04-28 19:44:06 +0000

    Quote

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,18 @@ import { Card } from "@/components/Card";
 import { Charity } from "@/components/Charity";
 import Donation from "@/components/Donation";
 import EmblaCarousel from "@/components/Hero";
-import Navbar from "@/components/Navbar";
+import Quotes from "@/components/Quotes";
 import { Testimonials } from "@/components/Testimonials";
 
 export default function Home() {
   return (
     <>
       <EmblaCarousel />
+      <Quotes />
       <Card />
       <Charity />
       <Donation />
+
       <Testimonials />
     </>
   );
